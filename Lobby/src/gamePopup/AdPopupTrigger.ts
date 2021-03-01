1249e9d4dc89795a845ed5e45ac083bb4641875c
Wayne <wayne@gamesmartltd.com>
2021-03-01 14:32:42 +0800

    ad trigger

---
+++ Lobby/src/gamePopup/AdPopupTrigger.ts
@@ -0,0 +1,41 @@
+class AdPopupTrigger {
+	public constructor() {
+	}
+
+	public static doWhateverYuoWant( trigger: Object ){
+		if( trigger ){
+			let click_behaviour: string = trigger["click_behaviour"];
+			if( click_behaviour ){
+				if (click_behaviour == "open_machine") {
+                    // open game parseInt(trigger["featured"])
+                }
+				else if (click_behaviour == "open_piggy") {
+					if (GlobelSettings["pigBankPoVo"]) {
+						// Trigger.showPigBank();
+						return;
+					}
+				}
+				// else if (click_behaviour == "open_loyalty") Trigger.insertModel(LoyaltyPopup);
+				// else if (click_behaviour == "open_questionnaire") Trigger.insertModel(QuestionsPopup);
+				else if (click_behaviour == "open_gift") {
+					// InviteAndGift.fromGift = true;
+					// Trigger.insertModel(InviteAndGift);
+				}
+				// else if (click_behaviour == "open_popup") Trigger.instance.showAdPopup();
+				// else if (click_behaviour == "open_po") Trigger.instance.showAdPo();
+				else if (click_behaviour == "open_wheel") {
+					Wheel.modal = SpinWheel.SpinWheelModel["COINS"];
+					// Trigger.insertModel(SpinWheelVIP);
+				}
+				else if (click_behaviour == "open_mission_bingo") {
+					// MissionVo.initMission("1");
+					// if (MissionVo.checkMissionLocked() < 0) Trigger.insertModel(MissionPopupForBingo);
+				}
+				else if (click_behaviour == "open_mission_slot") {
+					// MissionVo.initMission("2");
+					// if (MissionVo.checkMissionLocked() < 0) Trigger.insertModel(MissionPopupForSlot);
+				}
+			}
+		}
+	}
+}
